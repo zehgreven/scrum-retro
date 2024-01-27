89be947aafb055f125c2fdd8182b9f6fb2c66fea
José Lazaretti <eduardo1793@yahoo.com.br>
2024-01-27 15:40:10 -0300

    fix: carregando dados do tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+const tailWindColors = [
+  "bg-red-100",
+  "bg-red-300",
+  "bg-blue-100",
+  "bg-blue-300",
+  "bg-green-100",
+  "bg-green-300",
+  "bg-gray-100",
+  "bg-gray-300",
+];
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
